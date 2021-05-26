ac129083983e5b4e109be8e4d08dc9b593ab97ee
Ivan Trifonov <thereason12d@gmail.com>
2021-05-26 09:20:43 +0300

    added navbar,footer,about page and error page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Products,
+  Checkout,
+  Error,
+  About,
+  PrivateRoute,
+} from './pages/';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+  return (
+    <Router>
+      <Navbar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/product/:id'>
+          <SingleProduct />
+        </Route>
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route exact path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='About' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse,
+              praesentium hic quis itaque sapiente nostrum rem sint harum odit
+              mollitia corporis maxime nihil illum vel ducimus amet, ullam
+              cumque deserunt suscipit numquam animi ipsam facere qui
+              necessitatibus. Consequuntur eveniet est quia assumenda
+              laudantium? Iste, vel! Molestiae voluptas ad nihil ex?
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
