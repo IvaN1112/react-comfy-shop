9060a6faa853e50aa639f563b2689eb8b5a1dc0d
Ivan Trifonov <thereason12d@gmail.com>
2021-06-10 12:20:58 +0300

    added cart to local storage every time cart data changed, loaded cart from local storage on page load

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions';
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart');
+  if (cart) {
+    return JSON.parse(cart);
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   cart_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -24,6 +33,16 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
+  //remove item
+  const removeItem = (id) => {};
+  //toggleAmount
+  const toggleAmount = (id, value) => {};
+  //clear cart
+  const clearCart = () => {};
+  //add to local storage on cart change
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
     <CartContext.Provider value={{ ...state, addToCart }}>
       {children}
